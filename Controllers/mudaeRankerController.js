d732228697e359a3c35a4871d8f50ab6a4579ec1
LieutenantCrunch <52416950+LieutenantCrunch@users.noreply.github.com>
2019-07-06 15:53:20 -0400

    Added Get Left/Right Compare to controller scope
    Added the getLeft/RightCompare methods to the controller's scope object so I could get the objects in the Ranking Container for binding purposes

--- Controllers/mudaeRankerController.js
+++ Controllers/mudaeRankerController.js
@@ -3,5 +3,7 @@ mudaeRanker.controller('mudaeRankerController', ['$scope', '$http', 'Characters'
 $scope.characters = Characters.getCharacters();
 $scope.getModeClassName = Characters.getModeClassName;
 $scope.getNextModeName = Characters.getNextModeName;
+$scope.getLeftCompare = Characters.getLeftCompare;
+$scope.getRightCompare = Characters.getRightCompare;
 
 }]);
