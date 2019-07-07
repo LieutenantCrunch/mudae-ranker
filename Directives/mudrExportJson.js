7c86f18ea44321a1cc451987d836d76ad29bf557
LieutenantCrunch <52416950+LieutenantCrunch@users.noreply.github.com>
2019-07-06 21:56:04 -0400

    Utilities Output Update
    It's no longer necessary to pass the element in to exportSort since the Utilities service handles writing to the field with its new methods

--- Directives/mudrExportJson.js
+++ Directives/mudrExportJson.js
@@ -4,7 +4,7 @@ mudaeRanker.directive('mudrExportJson', ['Characters', function(Characters) {
 		scope: false,
 		link: function(scope, element, attrs) {
 			element.on('click', function(event) {
-				Characters.exportJson($('#JSONExport').first()[0]);
+				Characters.exportJson();
 			});
 		}
 	}
