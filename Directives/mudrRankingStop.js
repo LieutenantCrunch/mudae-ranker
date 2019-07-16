78c8ec3cb8cd16aa4f2ca85c3f17f53407fec0d0
LieutenantCrunch <52416950+LieutenantCrunch@users.noreply.github.com>
2019-07-16 09:50:17 -0400

    Stop Ranking Directive
    This is for the x button on the Ranking container so they stop in the middle of ranking

---
+++ Directives/mudrRankingStop.js
@@ -0,0 +1,12 @@
+mudaeRanker.directive('mudrRankingStop', ['Characters', function(Characters) {
+	return {
+		restrict: 'A',
+		scope: false,
+		link: function(scope, element, attrs) {
+			element.on('click', function(event) {
+				Characters.pauseRankMode();
+				scope.$apply();
+			});
+		}
+	}
+}]);
