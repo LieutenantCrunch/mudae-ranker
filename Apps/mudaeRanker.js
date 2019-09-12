cd14e666be99d87e1d1f3cf9b039fdf53e04ba0d
LieutenantCrunch <52416950+LieutenantCrunch@users.noreply.github.com>
2019-09-11 22:07:58 -0400

    RankChoice constant
    Added another constant for the rank choice

--- Apps/mudaeRanker.js
+++ Apps/mudaeRanker.js
@@ -1,3 +1,3 @@
 var mudaeRanker = angular.module('mudaeRanker', ['ng-sortable']);
 
-mudaeRanker.constant('Mode', { Edit: 0, Rank: 1}).constant('MergeCode', { NoAction: 0, NotFound: 1, Lookup: 2});
+mudaeRanker.constant('Mode', { Edit: 0, Rank: 1}).constant('MergeCode', { NoAction: 0, NotFound: 1, Lookup: 2}).constant('RankChoice', { Left: -1, Right: 1 });
